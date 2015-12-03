378ba95021e9c705d76dcd7f4aeda36112d82743
Joel Binnquist <joel.binnquist@gmail.com>
2015-12-03 21:10:34 +0100

    Host konfbar.

--- server.js
+++ server.js
@@ -9,12 +9,10 @@ const Scheduler = require('./Scheduler');
 const ZWave = require('./ZWave');
 const rp = require('request-promise');
 const argv = require('minimist')(process.argv.slice(2));
-console.log(argv);
+
+console.log('options:', argv);
 
 const schedule = YAML.load(argv.s || 'schedule.yaml').schedule;
-
-
-const zwave = new ZWave('razberry');
+const zwave = new ZWave(argv.h || 'localhost');
 const scheduler = new Scheduler(schedule, (s) => zwave.setSwitch(s));
-
 zwave.login().then(()=>scheduler.run());

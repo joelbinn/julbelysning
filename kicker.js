6853383329d8b5f39927dea83aa9ba85e2ffe35c
Joel Binnquist <joel.binnquist@gmail.com>
2015-12-05 16:00:01 +0100

    Fixa kicker.

--- kicker.js
+++ kicker.js
@@ -5,7 +5,7 @@ var intervalInMs = 1000 * 60 * 30;
 function kicker() {
   var now = new Date();
   console.log('Kick: ', now, 'next will occur at: ', new Date(now.getTime() + intervalInMs));
-  setTimeout(() => handleTimeout(), intervalInMs);
+  setTimeout(() => kicker(), intervalInMs);
 }
 
 module.exports = kicker;
